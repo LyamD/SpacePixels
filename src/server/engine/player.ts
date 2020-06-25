b36d13070fd6878cb1555e56839ea9bd9146063d
LyamD <lyam_durand@outlook.fr>
2020-06-25 02:24:01 +0200

    ajout début structure jeu coté serv

---
+++ src/server/engine/player.ts
@@ -0,0 +1,8 @@
+import { Position } from "./position";
+
+export class Player {
+    pos : Position
+    constructor(pos: Position) {
+        this.pos = pos;
+    }
+}
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,27 +1,38 @@
+//Server Imports
 import * as express from 'express';
 import * as socketio from 'socket.io';
-import * as matterjs from 'matter-js';
-const app = express();
+import * as path from 'path';
 
+// Game Import
+import * as matterjs from 'matter-js';
+import * as game from './game';
+import { Player } from './engine/player';
+import { Position } from './engine/position';
+
+//Création serveur express/node
+const app = express();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
-
-import * as path from 'path';
+//Changement path coté client
 app.use(express.static(path.join(__dirname + '/../client/')));//middleware
 
-// var game = require('./js/spacepixels/main.js');
-// const { Engine } = require("matter-js");
+//Création MatterJS engine
 var engine = matterjs.Engine.create();
-import * as game from './game';
-
 game.main(engine);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/index.html'));
 });
 
+//Liste des joueurs
+var PLAYERS : Player[] = [];
+
 io.on('connection', (socket : any) => {
-    console.log('player connected');
+    //Connection d'un joueur: On affiche son id
+    console.log('player connected : ' + socket.id);
+    //On créer son objet Player en pos 0,0
+    let player = new Player(new Position(0,0));
+    PLAYERS.push(player);
 
     socket.on('keyDown', (msg : string) => {
       io.emit('chat message', msg + 'pressed');
@@ -36,7 +47,7 @@ io.on('connection', (socket : any) => {
       });
 
     socket.on('disconnect', () => {
-        console.log('user disconnectedd');
+        console.log('user disconnected : ' + socket.id);
     });
 
 
